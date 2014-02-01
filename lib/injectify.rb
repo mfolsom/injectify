49f31ff52c4947919f3fdb3b265c0cea7da34bae
Megan Folsom <mfolsom@gmail.com>
2014-02-01 00:02:54 +0000

    minor cleanup

--- lib/injectify.rb
+++ lib/injectify.rb
@@ -1,12 +1,12 @@
 
-		class Array
-			def injectify value = nil
- 					if value.nil? 
- 					  value = self[0] 
- 						self[1..-1].each {|item| value = yield(value, item)}
-		      else
-		        self[0..-1].each { |item| value = yield(value, item)}
-		  	  end
-		   value
-			end
-		end
+class Array
+  def injectify value = nil
+    if value.nil? 
+       value = self[0] 
+       self[1..-1].each {|item| value = yield(value, item)}
+    else
+       self[0..-1].each { |item| value = yield(value, item)}
+    end
+  value
+  end
+end
--- spec/injectify_spec.rb
+++ spec/injectify_spec.rb
@@ -6,17 +6,17 @@ describe "injectify" do
     expect([5,6,7,8,9,10].injectify { |sum, n| sum + n }).to eq(45)
   end
 
-	it "takes a number passed to the method and prepends to the existing set" do
-		expect([1,2,4,5].injectify(2) {|sum,num| sum + num}).to eq(14)
-	end
+  it "takes a number passed to the method and prepends to the existing set" do
+    expect([1,2,4,5].injectify(2) {|sum,num| sum + num}).to eq(14)
+  end
 
-	it "handles a 1 as the first prepended number" do
-		expect([1,2,4,5].injectify(1) {|sum,num| sum + num}).to eq(13)
-	end
+  it "handles a 1 as the first prepended number" do
+    expect([1,2,4,5].injectify(1) {|sum,num| sum + num}).to eq(13)
+  end
 
-	it "handles a 0 as the first prepended number" do
-		expect([1,2,4,5].injectify(0) {|sum,num| sum + num}).to eq(12)
-	end
+  it "handles a 0 as the first prepended number" do
+    expect([1,2,4,5].injectify(0) {|sum,num| sum + num}).to eq(12)
+  end
 
   it "multiplies some numbers" do
    expect([5,6,7,8,9,10].injectify(1) { |product, n| product * n }).to eq(151200)
